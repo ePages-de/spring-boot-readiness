a3343855766cbdbf1fcafeed9777ed50502f80d6
Jens Fischer <soth@gmx.net>
2017-09-05 14:08:28 +0200

    Bootstrap Thymeleaf hacking

--- src/main/java/com/epages/readiness/Response.java
+++ src/main/java/com/epages/readiness/Response.java
@@ -1,14 +1,14 @@
 package com.epages.readiness;
 
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+import static org.springframework.boot.actuate.health.Status.UP;
+
+import org.springframework.boot.actuate.health.Status;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
-
-import org.springframework.boot.actuate.health.Status;
-
-import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
-import static org.springframework.boot.actuate.health.Status.UP;
 
 @JsonInclude(NON_NULL)
 @JsonPropertyOrder(value = {"status", "platform", "uri", "totalTimeMillis", "description"}, alphabetic = true)
@@ -22,5 +22,17 @@ public interface Response {
     @JsonIgnore
     default boolean isUp() {
         return UP.equals(getStatus());
+    }
+
+    @JsonIgnore
+    default String getCssClass() {
+        switch (getStatus().getCode()) {
+            case "UP":
+                return "success";
+            case "DEGRADED":
+                return "warning";
+            default:
+                return "danger";
+        }
     }
 }
